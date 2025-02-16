4fe567b8c2d4efa7235d1f370cd0ab027a38a478
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-16 18:47:14 +1100

    getHumanChoice implementation

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ console.log("Js Linked correctly");
 // ---playGame
 // initialise rounds to 5
 // initialise humanScore, computerScore to zero 
-// while both humanScore and computerScore < 5 
+// while both humanScore and computerScore < rounds 
 // 		CALL getHumanChoice
 // 		CALL getComputerChoice 
 // 		if humanChoice === computerChoice, display draw message
 //		if player wins, increment player score by 1 and display message
 //		if computer wins, increment computer score by 1 and display message
-// if player score is 5, display win message
+// if player score is rounds, display win message
 // otherwise display loss message
 
 // ---getHumanChoice
-// initialise choice to empty string 
+// initialise choice to empty string
 // while choice is empty string
 //   	prompt user to pick rock, paper or scissors
 //		if prompt is equal to rock, paper or scissors, set choice to it
@@ -28,3 +28,21 @@ console.log("Js Linked correctly");
 // if seed < 1/3 return "rock"
 // if seed < 2/3 return "paper"
 // return "scissors"
+
+
+// prompt user for choice of rock, paper or scissors until valid input encountered
+function getHumanChoice() {
+	let choice = "";
+
+	while (choice === "") {
+		// convert to lower case to allow for case insensitive input
+		let tmp = prompt("Please type rock, paper or scissors").toLowerCase();
+
+		if ((tmp === "rock") || (tmp === "paper") || (tmp === "scissors")) {
+			choice = tmp;
+		}
+	}
+	return choice;
+}
+
+
